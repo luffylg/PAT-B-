f8d2e039d9e35742367e8fb25185c0605907390d
luffylg <luffylg05@gmail.com>
2016-10-11 23:19:39 +0800

    improved

--- solution_1003.java
+++ solution_1003.java
@@ -20,35 +20,25 @@ public class solution_1003 {
 
 	private static Boolean pass(String s) {
 		// TODO Auto-generated method stub
+		int len=s.length();
+		if (len<3) return false;
 		int a1=0;
 		int n=0;
-		int point=0;
-		for (int i = 0; i < s.length(); i++) {
-			point++;
+		int p=0;
+		int t=0;
+		int ts=0;
+		for (int i = 0; i < len; i++) {
 			char val = s.charAt(i);
-			if (val=='P') {
+			if (val=='P'){
+				p++;
 				a1=i;
-				break;
-			}
-			if (val!='A') return false;
-			
+			}else if (val=='T') {
+				t++;
+				ts=i;
+			}else if (val!='A') return false;
 		}
-		for (int j = point; j < s.length(); j++){	
-			point++;
-			char val = s.charAt(j);
-			if (val=='T'){
-				n=j-a1-2;
-				if (n<0) return false;
-				break;
-			}
-			if (val!='A') return false;
-		}
-		for (int i = point; i < s.length(); i++) {
-			char val = s.charAt(i);
-			if (val!='A') return false;
-		}
-		return s.length()-point==(n+1)*a1;
+		n=ts-a1-1;
+		if (t!=1 || p!=1 || n<0) return false;
+		return n*a1==(len-ts-1);
 	}
-
-
 }
