171d50b8ce84acea22b6c1873e295f3dc8f418b3
luffylg <luffylg05@gmail.com>
2016-10-13 23:05:34 +0800

    1013 improved

--- solution_1013.java
+++ solution_1013.java
@@ -1,5 +1,6 @@
 package patb_java;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class solution_1013 {
@@ -11,32 +12,34 @@ public class solution_1013 {
 		int m=in.nextInt();
 		in.close();
 		if (n==1) {
-			System.out.print(2);
+			System.out.printf("%d",2);
 		}
 		int num=3;
 		int count=1;
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		list.add(2);
 		while (count<m) {
-			if (issu(num)) {
+			if (issu(num,list)) {
+				list.add(num);
 				if(++count<n) {
 					num+=2;
 					continue;
 				}
 				int ca=(count-n)%10;
 				if (ca==0) {
-					System.out.print(num);
+					System.out.printf("%d",num);
 				}else if (ca==9) {
 					System.out.println(" "+num);
 				}else {
-					System.out.print(" "+num);
+					System.out.printf(" %d",num);
 				}
 			}
 			num+=2;
 		}
 	}
 
-	private static boolean issu(int num) {
-		int len=(int) Math.sqrt(num);
-		for (int i = 3; i <= len; i+=2) {
+	private static boolean issu(int num, ArrayList<Integer> list) {
+		for (int i:list) {
 			if (num%i==0) {
 				return false;
 			}
