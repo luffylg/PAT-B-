834c415c4401cfe5496d65142a27b5925b701da0
luffylg <lake0105@foxmail.com>
2016-10-18 22:01:52 +0800

    solution 1024

--- solution_1024.java
+++ solution_1024.java
@@ -1,15 +1,59 @@
 package patb;
 
-import java.math.BigDecimal;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class solution_1024 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);  
-	    BigDecimal s = sc.nextBigDecimal();  
-	    System.out.println(s.toPlainString());  
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		String line = reader.readLine();
+		reader.close();
+		char zhengfu=line.charAt(0);
+		System.out.print(zhengfu=='-'?zhengfu:"");
+		char zhengshu=line.charAt(1);
+		String xiaoshu="";
+		for (int i = 3; i < line.length(); i++) {
+			char c=line.charAt(i);
+			if (c=='E') {
+				break;
+			}
+			xiaoshu+=c;
+		}
+		int xiaoshul=xiaoshu.length();
+		char chengchu=line.charAt(xiaoshul+4);
+		String mici="";
+		for (int i = xiaoshul+5; i < line.length(); i++) {
+			char c=line.charAt(i);
+			mici+=c;
+		}
+		int mi=Integer.valueOf(mici);
+		if (mi==0) {
+			System.out.println(zhengshu+'.'+xiaoshu);
+		}else if (chengchu=='+') {
+			System.out.print(zhengshu);
+			if (mi>=xiaoshul) {
+				System.out.print(xiaoshu);
+				for (int i = 0; i < mi-xiaoshul; i++) {
+					System.out.print('0');
+				}
+			}else {
+				for (int i = 0; i < xiaoshul; i++) {
+					char c=xiaoshu.charAt(i);
+					System.out.print(c);
+					if (i==mi-1) {
+						System.out.print('.');
+					}
+				}
+			}
+		}else {
+			System.out.print("0.");
+			for (int i = 0; i < mi-1; i++) {
+				System.out.print('0');
+			}
+			System.out.print(zhengshu+xiaoshu);
+		}
 	}
-
 }
