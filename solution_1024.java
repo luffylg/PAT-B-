6c06d6f93cd846fa1cb69dbe8961bdb1386bec94
luffylg <lake0105@foxmail.com>
2016-10-18 20:58:23 +0800

    solution 1024

---
+++ solution_1024.java
@@ -0,0 +1,15 @@
+package patb;
+
+import java.math.BigDecimal;
+import java.util.Scanner;
+
+public class solution_1024 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);  
+	    BigDecimal s = sc.nextBigDecimal();  
+	    System.out.println(s.toPlainString());  
+	}
+
+}
