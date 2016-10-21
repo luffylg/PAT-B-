f1278806eec95f8de2bb8ddb0e8e9dfb4d295ef3
luffylg <lake0105@foxmail.com>
2016-10-21 20:54:47 +0800

    solution 1028 last test overtime

--- solution_1028.java
+++ solution_1028.java
@@ -3,27 +3,45 @@ package patb;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
 
 public class solution_1028 {
-
+	public static int cp(String b1,String b2){
+		String[] split1 = b1.split("/");
+		String[] split2 = b2.split("/");
+		if (Integer.valueOf(split1[0])-Integer.valueOf(split2[0])!=0) {
+			return Integer.valueOf(split2[0])-Integer.valueOf(split1[0]);
+		}
+		if (Integer.valueOf(split1[1])-Integer.valueOf(split2[1])!=0) {
+			return Integer.valueOf(split2[1])-Integer.valueOf(split1[1]);
+		}
+		return Integer.valueOf(split2[2])-Integer.valueOf(split1[2]);
+	}
+	
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 		int sum=Integer.valueOf(reader.readLine());
 		int count=0;
-		person young=new person("", "2014/09/06");
-		person old=new person("", "1814/09/06");
-		person lao=new person("", "2014/09/06");
-		person nianq=new person("", "1814/09/06");
+		String young="2014/09/06";
+		String old="1814/09/06";
+		String lao="2014/09/06";
+		String nianq="1814/09/06";
+		String laoname="";
+		String xiaoname="";
 		for (int i = 0; i < sum; i++) {
 			String[] split=reader.readLine().split(" ");
-			person p=new person(split[0], split[1]);
-			if (p.compareTo(young)>=0&&p.compareTo(old)<=0) {
-				lao=p.compareTo(lao)>0?p:lao;
-				nianq=p.compareTo(nianq)<0?p:nianq;
+			String name=split[0];
+			String birthday=split[1];
+			if (cp(birthday, young)>=0&&cp(birthday, old)<=0) {
 				count++;
+				if (cp(birthday, lao)>0) {
+					lao=birthday;
+					laoname=name;
+				}
+				if (cp(birthday, nianq)<0) {
+					nianq=birthday;
+					xiaoname=name;
+				}
 			}
 			
 		}
@@ -31,31 +49,7 @@ public class solution_1028 {
 		if (count==0) {
 			System.out.println(0);
 		}else {
-			System.out.println(count+" "+lao.name+" "+nianq.name);
+			System.out.println(count+" "+laoname+" "+xiaoname);		
 		}
 	}
-	private static class person implements Comparable<person>{
-		String name;
-		String birthday;
-		public person(String name, String birthday) {
-			this.name = name;
-			this.birthday = birthday;
-		}
-		@Override
-		public int compareTo(person o) {
-			// TODO Auto-generated method stub
-			String[] split1 = this.birthday.split("/");
-			String[] split2 = o.birthday.split("/");
-			if (Integer.valueOf(split1[0])-Integer.valueOf(split2[0])!=0) {
-				return Integer.valueOf(split2[0])-Integer.valueOf(split1[0]);
-			}
-			if (Integer.valueOf(split1[1])-Integer.valueOf(split2[1])!=0) {
-				return Integer.valueOf(split2[1])-Integer.valueOf(split1[1]);
-			}
-			return Integer.valueOf(split2[2])-Integer.valueOf(split1[2]);
-		}
-		
-		
-	}
-
 }
